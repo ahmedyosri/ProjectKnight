4b580ddf2a40e757dd343cc014230ac8e46305c8
ahmedyosri <ahmed.yossre@gmail.com>
2015-11-18 13:00:39 +0200

    Fixed Wall jumping funtions

---
+++ Source/Knight3/Faye.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "SideScrollerCharacter.h"
+#include "Faye.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KNIGHT3_API AFaye : public ASideScrollerCharacter
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
